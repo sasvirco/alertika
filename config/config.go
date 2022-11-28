df386c179fb3367e5f6401d21f36a887103add86
Aleksandar Zhelyazkov <azhelyazkov@ep.com>
2022-11-27 17:07:41 -0800

    formatting

--- config/config.go
+++ config/config.go
@@ -7,13 +7,13 @@ import (
 	"github.com/pelletier/go-toml"
 )
 
-//GeneralConfig - contains general configuration values for clooudwatch search
+// GeneralConfig - contains general configuration values for clooudwatch search
 type GeneralConfig struct {
 	RunInterval string `toml:"run_interval"`
 	Template    string `toml:"sns_message_template"`
 }
 
-//Rule - contains rules for clooudwatch search
+// Rule - contains rules for clooudwatch search
 type Rule struct {
 	Name      string `toml:"name"`
 	LogGroup  string `toml:"log_group"`
@@ -24,13 +24,13 @@ type Rule struct {
 	NotifyOk  bool   `toml:"notify_ok,omitempty"`
 }
 
-//Config - config structure
+// Config - config structure
 type Config struct {
 	General GeneralConfig   `toml:"general"`
 	Rules   map[string]Rule `toml:"rules"`
 }
 
-//ParseConfig parses toml config and returns it back as struct
+// ParseConfig parses toml config and returns it back as struct
 func ParseConfig(path string) (*Config, error) {
 	filename, _ := filepath.Abs(path)
 	tomlfile, err := ioutil.ReadFile(filename)
